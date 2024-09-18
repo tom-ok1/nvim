61c7286bd7efc606c6c11ffbb33e7e3ebec1410a
sasakitimaru <tom.ok1@icloud.com>
2024-09-19 08:03:18 +0900

    add live_grep

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -26,6 +26,10 @@ return {
           -- toggle term
           -- https://github.dev/AstroNvim/AstroNvim > lua > astronvim > plugins > toggleterm.lua
           ["<C-'>"] = { '<Cmd>execute v:count . "ToggleTerm"<CR>', desc = "Toggle terminal" },
+          ["<Leader>fl"] = {
+            function() require("telescope").extensions.live_grep_args.live_grep_args() end,
+            desc = "Live grep args",
+          },
         },
         t = {
           -- toggle term

62ad10033ec76521f5568937f61c460f6c437f58
tom-ok1 <tom.ok1@icloud.com>
2024-10-01 20:08:14 +0900

    Fix: set scope explicitly
    https://gist.github.com/sys9kdr/3e57e650deda1301a45dfe5873e68dd4

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_option_value("ignorecase", true)
-vim.api.nvim_set_option_value("smartcase", true)
+vim.api.nvim_set_option_value("ignorecase", true, { scope = "local" })
+vim.api.nvim_set_option_value("smartcase", true, { scope = "local" })

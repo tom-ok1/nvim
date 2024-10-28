2ee4e1b1670dc696eb3ae79209a732895e48b87b
tom-ok1 <tom.ok1@icloud.com>
2024-10-28 15:57:22 +0900

    Fix to load scrollbar after gitsigns initialization

--- lua/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
-    require("scrollbar.handlers.gitsigns").setup()
-  end,
-}
--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,4 +1,8 @@
 return {
   "petertriho/nvim-scrollbar",
-  config = function() require("scrollbar").setup() end,
+  dependencies = { "lewis6991/gitsigns.nvim" },
+  config = function()
+    require("scrollbar").setup()
+    require("scrollbar.handlers.gitsigns").setup()
+  end,
 }

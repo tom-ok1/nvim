20eca252539cd06aec9ad27247f1b6d27f600d9a
tom-ok1 <tom.ok1@icloud.com>
2024-11-22 15:45:02 +0900

    chore: adjust scroll speed

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,4 +1,22 @@
 return {
   "karb94/neoscroll.nvim",
-  config = function() require("neoscroll").setup {} end,
+  config = function()
+    local neoscroll = require "neoscroll"
+    neoscroll.setup()
+    local keymap = {
+      ["<C-u>"] = function() neoscroll.ctrl_u { duration = 50 } end,
+      ["<C-d>"] = function() neoscroll.ctrl_d { duration = 50 } end,
+      ["<C-b>"] = function() neoscroll.ctrl_b { duration = 50 } end,
+      ["<C-f>"] = function() neoscroll.ctrl_f { duration = 50 } end,
+      ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 50 }) end,
+      ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 50 }) end,
+      ["zt"] = function() neoscroll.zt { half_win_duration = 50 } end,
+      ["zz"] = function() neoscroll.zz { half_win_duration = 50 } end,
+      ["zb"] = function() neoscroll.zb { half_win_duration = 50 } end,
+    }
+    local modes = { "n", "v", "x" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
+  end,
 }

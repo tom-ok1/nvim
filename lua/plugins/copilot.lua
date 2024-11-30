b8eb622641ed8679ead0c3f9adba73747a5c3049
tom-ok1 <tom.ok1@icloud.com>
2024-11-30 22:58:46 +0900

    add copilotchat

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {},
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
